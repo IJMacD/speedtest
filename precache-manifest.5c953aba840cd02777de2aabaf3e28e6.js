aba3fec5b0cee065062c9e03b2b97d2935cf4df3
Iain MacDonald <IJMacD@gmail.com>
2020-08-21 02:14:17 +0800

    Updates

---
+++ precache-manifest.5c953aba840cd02777de2aabaf3e28e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce66c36529c432599a0d061c591db0d2",
+    "url": "/speedtest/index.html"
+  },
+  {
+    "revision": "26d2eaf793b5228e3a98",
+    "url": "/speedtest/static/css/main.b3cc0087.chunk.css"
+  },
+  {
+    "revision": "1800c8133dfd10518cf5",
+    "url": "/speedtest/static/js/2.23e54e54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/speedtest/static/js/2.23e54e54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26d2eaf793b5228e3a98",
+    "url": "/speedtest/static/js/main.739bb284.chunk.js"
+  },
+  {
+    "revision": "01957ab73732b7403afe",
+    "url": "/speedtest/static/js/runtime-main.c7326d98.js"
+  },
+  {
+    "revision": "4b033b2e40148aa42e5c85566906f329",
+    "url": "/speedtest/static/media/logo.4b033b2e.svg"
+  },
+  {
+    "revision": "e8abaee4013c33e0745c67ebc72edaf9",
+    "url": "/speedtest/static/media/pinging.e8abaee4.svg"
+  }
+]);
